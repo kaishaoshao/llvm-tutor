163aefd35fa8647e6c7f5ac5964937a9fe7bf98e
shaokai.wang <shaokai.wang@terapines.com>
2024-11-14 17:37:42 +0800

    build.sh: 规范化 CMakeLists.txt: 适应通用环境

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ script_dir="$(dirname "$(readlink -f "$0")")"
 build_dir="${script_dir}/build"
 llvm_local_dir="/usr/local/"
 mkdir -p ${build_dir}
-cmake -B "${build_dir}"                 \
+cmake -B "${build_dir}"                        \
       -DLT_LLVM_INSTALL_DIR=${llvm_local_dir}  \
        && make -C "${build_dir}" -j4
 
